26e6189be8158522ef272ec5f57101482ec7337a
yangzhe1991 <ud1937@gmail.com>
2013-08-25 17:41:15 +0800

    继续开发中

--- main/index.go
+++ main/index.go
@@ -11,13 +11,14 @@ var tmpCredDB map[string]*ynote.Credentials = make(map[string]*ynote.Credentials
 
 var globalSessions *session.Manager
 
+
 func index(rw http.ResponseWriter, req *http.Request) {
 	yc:=ynote.NewOnlineYnoteClient(ynote.Credentials{
 		Token:  Token,
 		Secret: Secret})
 	sess:=globalSessions.SessionStart(rw,req)
 	defer sess.SessionRelease()
-	log.Print("/index")
+	log.Print("/")
 
 	cred,ok:=sess.Get("accToken").(ynote.Credentials)
 
@@ -26,6 +27,7 @@ func index(rw http.ResponseWriter, req *http.Request) {
 		log.Print("no accToken in session")
 		tmpCred, err := yc.RequestTemporaryCredentialsWithCallBack("http://127.0.0.1:8099/note/callback")
 		if err != nil {
+			http.Redirect(rw,req,"/note/error",301)
 			return
 		}
 		tmpCredDB[tmpCred.Token]=tmpCred
@@ -36,15 +38,96 @@ func index(rw http.ResponseWriter, req *http.Request) {
 	}
 	log.Print(cred)
 	yc.AccToken=&cred
-	ui,err:=yc.UserInfo()
+	_,err:=yc.UserInfo()
 	if err!=nil{
-		fmt.Fprint(rw,"error!")
+		http.Redirect(rw,req,"/note/error",301)
+		return
 	}
 
-	fmt.Fprint(rw,ui)
+
+	notebooks,err:=yc.ListNotebooks()
+	if err!=nil {
+		http.Redirect(rw,req,"/note/error",301)
+		return
+	}
+	var mynotebook ynote.NotebookInfo=nil
+	for _,notebook := range notebooks{
+		if notebook.Name=="第三方应用" {
+			mynotebook=notebook
+		}
+	}
+	if mynotebook==nil{
+		mynotebook,err=yc.CreateNotebook("第三方应用","")
+		if err!=nil{
+			http.Redirect(rw,req,"/note/error",301)
+			return
+		}
+	}
+	notes,err:=yc.ListNotes(mynotebook.Path)
+	if err!=nil{
+		http.Redirect(rw,req,"/note/error",301)
+		return
+	}
+	fmt.Fprintf(rw,html1)
+	for _,notepath:=range notes{
+		note,err:=yc.NoteInfo(notepath)
+		if err!=nil{
+			http.Redirect(rw,req,"/note/error",301)
+			return
+		}
+		fmt.Fprintf(rw,"<a href=\"/note/edit?path=%s\">%s</a><br>",notepath,note.Title)
+	}
+	fmt.Fprint(rw,"<a href=\"/note/create\">create a new note</a><br>")
+
+	fmt.Fprintf(rw,html2)
+}
+
+
+func edit(rw http.ResponseWriter, req *http.Request) {
+	yc:=ynote.NewOnlineYnoteClient(ynote.Credentials{
+		Token:  Token,
+		Secret: Secret})
+	sess:=globalSessions.SessionStart(rw,req)
+	defer sess.SessionRelease()
+	log.Print("/edit")
+	cred,ok:=sess.Get("accToken").(ynote.Credentials)
+
+	if !ok||req.FormValue("path")=="" {
+		log.Print("no accToken in session")
+		http.Redirect(rw,req,"/note",301)
+		return
+	}
+
+	yc.AccToken=cred
+
+	note,err:=yc.NoteInfo(req.FormValue("path"))
+	if err!=nil{
+		http.Redirect(rw,req,"/note/error",301)
+		return
+	}
+	if req.Method=="GET"{
+		fmt.Fprintf(rw,html1)
+		fmt.Fprint(rw,
+			`
+			<form>
+			title<br>
+			 <input name="title" />
+			`
+		)
+		fmt.Fprint(rw,html2)
+	}
 
 
 
+
+
+}
+
+func error(rw http.ResponseWriter, req *http.Request) {
+	log.Print("/error")
+	fmt.Fprintf(rw,html1)
+	fmt.Fprintf(rw,"有地方出错了……")
+	fmt.Fprintf(rw,html2)
 }
 
 
@@ -80,6 +163,9 @@ func main(){
 	globalSessions, _ = session.NewManager("memory", "session", 3600,"")
 	go globalSessions.GC()
 	http.HandleFunc("/note/callback",callback)
+	http.HandleFunc("/note/edit",edit)
+	http.HandleFunc("/note/error",error)
+	http.HandleFunc("/note/create",create)
 	http.HandleFunc("/note/",index)
 	http.HandleFunc("/latex/",latex)
 	log.Print("service start")
@@ -91,3 +177,23 @@ func main(){
 
 }
 
+var html1 string = `<html>
+				<head>
+				<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+				<title>无名应用</title>
+				</head>
+				<body>`
+var html2 string= `<script type="text/javascript">
+				  var _gaq = _gaq || [];
+				  _gaq.push(['_setAccount', 'UA-29543335-1']);
+				  _gaq.push(['_setDomainName', 'yangzhe1991.org']);
+				  _gaq.push(['_trackPageview']);
+				  (function() {
+					var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+				  })();
+				</script>
+				</body>
+
+				</html>`
